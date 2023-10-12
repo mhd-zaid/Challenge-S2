063dad9e7836bb81c8550599573f177cd02f8aee
Rafael93150 <tavaresrafael93@gmail.com>
2023-10-12 16:35:58 +0200

    add user stats routes and update dash data

--- api/src/router/statsRouter.js
+++ api/src/router/statsRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getNewUsers } from "../routes/statsRoutes.js";
+import { getNewUsersBeforeLast30Days, getNewUsersLast30Days, getNewUsersLastYear } from "../routes/statsRoutes.js";
 
 const router = express.Router();
 
-router.post("/registrations", getNewUsers);
+router.get("/registrations/last-30-days", getNewUsersLast30Days);
+router.get("/registrations/before-last-30-days", getNewUsersBeforeLast30Days);
+router.get("/registrations/last-year", getNewUsersLastYear);
 
 export default router;
--- api/src/routes/statsRoutes.js
+++ api/src/routes/statsRoutes.js
@@ -1,26 +1,101 @@
 import User from "../models/mongodb-user.js";
 
-export const getNewUsers = async (req, res) => {
+export const getNewUsersLast30Days = async (req, res) => {
 	try {
-		const dates = req.body.dates;
-		if (
-			!dates ||
-			dates.length !== 2 ||
-			isNaN(Date.parse(dates[0])) ||
-			isNaN(Date.parse(dates[1]))
-		)
-			throw new Error("Invalid arguments");
-        dates[1] = new Date(dates[1]).setHours(23, 59, 59, 999);
-		const users = await User.find({
+		const currentDate = new Date();
+		const date30DaysAgo = new Date(
+			currentDate.setDate(currentDate.getDate() - 30)
+		);
+		date30DaysAgo.setHours(0, 0, 0, 0);
+		const query = {
 			createdAt: {
-				$gte: new Date(dates[0]),
-				$lte: new Date(dates[1]),
+				$gte: date30DaysAgo,
+				$lte: new Date(),
 			},
-		}).countDocuments();
-		res.json(users);
+		};
+		const usersCount = await User.find(query).countDocuments();
+		res.status(200).json(usersCount);
 	} catch (error) {
 		res.status(500).json({
-			error: `An error occurred while retrieving the new users : ${error}`,
+			error: `An error occurred while retrieving the new users: ${error}`,
+		});
+	}
+};
+
+export const getNewUsersBeforeLast30Days = async (req, res) => {
+	try {
+		const currentDate = new Date();
+		const date30DaysAgo = new Date(currentDate);
+		date30DaysAgo.setDate(currentDate.getDate() - 60);
+		date30DaysAgo.setHours(0, 0, 0, 0);
+
+		const date30DaysAgoEnd = new Date(currentDate);
+		date30DaysAgoEnd.setDate(currentDate.getDate() - 30);
+		date30DaysAgoEnd.setHours(23, 59, 59, 999);
+
+		const query = {
+			createdAt: {
+				$gte: date30DaysAgo,
+				$lte: date30DaysAgoEnd,
+			},
+		};
+
+		const usersCount = await User.find(query).countDocuments();
+		res.status(200).json(usersCount);
+	} catch (error) {
+		res.status(500).json({
+			error: `An error occurred while retrieving the new users: ${error}`,
+		});
+	}
+};
+
+export const getNewUsersLastYear = async (req, res) => {
+	try {
+		const currentDate = new Date();
+		const months = [
+			"Janvier",
+			"Février",
+			"Mars",
+			"Avril",
+			"Mai",
+			"Juin",
+			"Juillet",
+			"Août",
+			"Septembre",
+			"Octobre",
+			"Novembre",
+			"Décembre",
+		];
+		const lastMonths = [];
+		const newUsers = [];
+		for (let i = 0; i < 12; i++) {
+			const date = new Date(
+				currentDate.setMonth(currentDate.getMonth() - i)
+			);
+			const startDate = new Date(date.getFullYear(), date.getMonth(), 1);
+			const endDate = new Date(
+				date.getFullYear(),
+				date.getMonth() + 1,
+				0
+			);
+			endDate.setHours(23, 59, 59, 999);
+			const query = {
+				createdAt: {
+					$gte: startDate,
+					$lte: endDate,
+				},
+			};
+			const users = await User.find(query).countDocuments();
+			lastMonths.push(months[date.getMonth()]);
+			newUsers.push(users);
+		}
+		res.status(200).json({
+			months: lastMonths.reverse(),
+			newUsers: newUsers.reverse(),
+		});
+	} catch (error) {
+		res.status(500).json({
+			error: `An error occurred while retrieving the new users: ${error}`,
 		});
 	}
 };
