995ca17c7403389261043ac27f3b7e56814e8d8e
Rafael93150 <tavaresrafael93@gmail.com>
2023-10-16 14:33:36 +0200

    git add products model and stats routes + update dash

--- api/src/router/statsRouter.js
+++ api/src/router/statsRouter.js
@@ -1,10 +1,22 @@
 import express from "express";
-import { getNewUsersBeforeLast30Days, getNewUsersLast30Days, getNewUsersLastYear } from "../routes/statsRoutes.js";
+import {
+	getNewUsersBeforeLast30Days,
+	getNewUsersLast30Days,
+	getNewUsersLastYear,
+	getNewProductsBeforeLast30Days,
+	getNewProductsLast30Days,
+	getNewProductsLastYear,
+    getLastMonths,
+} from "../routes/statsRoutes.js";
 
 const router = express.Router();
 
+router.get("/months", getLastMonths);
 router.get("/registrations/last-30-days", getNewUsersLast30Days);
 router.get("/registrations/before-last-30-days", getNewUsersBeforeLast30Days);
 router.get("/registrations/last-year", getNewUsersLastYear);
+router.get("/products/last-30-days", getNewProductsLast30Days);
+router.get("/products/before-last-30-days", getNewProductsBeforeLast30Days);
+router.get("/products/last-year", getNewProductsLastYear);
 
 export default router;
--- dashboard/src/views/DashboardView.vue
+++ dashboard/src/views/DashboardView.vue
@@ -2,21 +2,26 @@
 import AuthenticatedLayout from '@/layouts/AuthenticatedLayout.vue'
 import axiosInstance from '@/utils/axiosInstance'
 import { Bar } from 'vue-chartjs'
-import DashIncreaseBlock from '@/components/dashboard/DashIncreaseBlock.vue'
+import MiniStatisticsCard from '@/components/dashboard/MiniStatisticsCard.vue'
 import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
   BarElement,
   CategoryScale,
-  LinearScale
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip
 } from 'chart.js'
-import { reactive } from 'vue'
+import { onBeforeMount, reactive } from 'vue'
+import { ArchiveBoxIcon, ShoppingBagIcon, UserPlusIcon } from '@heroicons/vue/24/outline'
 
 const state = reactive({
   newUsersLast30Days: 0,
   newUsersAugmentation: 0,
+  newProductsLast30Days: 0,
+  newProductsAugmentation: 0,
+  newOrdersLast30Days: 1973,
+  newOrdersAugmentation: -176,
   userChartData: {
     labels: [],
     datasets: [
@@ -32,12 +37,12 @@ const state = reactive({
       }
     ]
   },
-  orderChartData: {
+  productChartData: {
     labels: [],
     datasets: [
       {
-        data: [576, 893, 923, 1239, 748, 689, 908, 1389, 879, 1087, 1234, 1973],
-        label: 'Nouvelles commandes',
+        data: [],
+        label: 'Nouveaux produits',
         backgroundColor: '#6366F1',
         borderColor: '#6366F1',
         borderWidth: 1,
@@ -47,40 +52,71 @@ const state = reactive({
       }
     ]
   },
+  orderChartData: {
+    labels: [],
+    datasets: [
+      {
+        data: [576, 893, 923, 1239, 748, 689, 908, 1389, 879, 1087, 1234, 1973],
+        label: 'Nouvelles commandes',
+        backgroundColor: '#d97706',
+        borderColor: '#d97706',
+        borderWidth: 1,
+        barThickness: 20,
+        borderRadius: 4,
+        hoverBackgroundColor: '#b45309'
+      }
+    ]
+  },
   chartOptions: {
-    responsive: true
+    responsive: true,
+    scales: {
+      y: {
+        suggestedMax: 10
+      }
+    }
   }
 })
 
 let usersPerMonthKey = 0
-let orderPerMonthKey = 0
+let ordersPerMonthKey = 0
+let productsPerMonthKey = 0
 
-const getData = async () => {
+onBeforeMount(async () => {
   try {
-    // Users data
-    const newUsersLastYear: any = await axiosInstance.get('/stats/registrations/last-year')
-    state.userChartData.labels = newUsersLastYear.data.months
-    state.userChartData.datasets[0].data = newUsersLastYear.data.newUsers
+    const [
+      newUsersLast30Days,
+      newUsersBeforeLast30Days,
+      newProductsLast30Days,
+      newProductsBeforeLast30Days,
+      months,
+      newUsersLastYear,
+      newProductsLastYear
+    ] = await Promise.all([
+      axiosInstance.get('/stats/registrations/last-30-days'),
+      axiosInstance.get('/stats/registrations/before-last-30-days'),
+      axiosInstance.get('/stats/products/last-30-days'),
+      axiosInstance.get('/stats/products/before-last-30-days'),
+      axiosInstance.get('/stats/months'),
+      axiosInstance.get('/stats/registrations/last-year'),
+      axiosInstance.get('/stats/products/last-year')
+    ])
 
-    const newUsersLast30Days: any = await axiosInstance.get('/stats/registrations/last-30-days')
     state.newUsersLast30Days = newUsersLast30Days.data
-
-    const newUsersBeforeLast30Days: any = await axiosInstance.get(
-      '/stats/registrations/before-last-30-days'
-    )
     state.newUsersAugmentation = newUsersLast30Days.data - newUsersBeforeLast30Days.data
-
-    // Orders data
-    state.orderChartData.labels = newUsersLastYear.data.months
-
+    state.newProductsLast30Days = newProductsLast30Days.data
+    state.newProductsAugmentation = newProductsLast30Days.data - newProductsBeforeLast30Days.data
+    state.userChartData.labels = months.data
+    state.orderChartData.labels = months.data
+    state.productChartData.labels = months.data
+    state.userChartData.datasets[0].data = newUsersLastYear.data
+    state.productChartData.datasets[0].data = newProductsLastYear.data
     usersPerMonthKey++
-    orderPerMonthKey++
+    productsPerMonthKey++
+    ordersPerMonthKey++
   } catch (error) {
     console.log(error)
   }
 }
-
-getData()
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 </script>
@@ -91,32 +127,30 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
       <h1 class="text-lg leading-6 font-semibold text-gray-900">Tableau de bord</h1>
       <h4 class="mt-8 text-base font-medium leading-6 text-gray-900">30 derniers jours</h4>
       <dl class="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-        <DashIncreaseBlock
-          :kind="'users'"
+        <MiniStatisticsCard
+          :icon="UserPlusIcon"
           label="Nouvelles inscriptions"
           :data="state.newUsersLast30Days"
           :increase="state.newUsersAugmentation"
         />
-        <DashIncreaseBlock
-          :kind="'orders'"
-          label="Nouvelles commandes"
-          :data="1973"
-          :increase="176"
-        />
-        <DashIncreaseBlock
-          :kind="'products'"
+        <MiniStatisticsCard
+          :icon="ArchiveBoxIcon"
           label="Nouveaux produits"
-          :data="212"
-          :increase="32"
+          :data="state.newProductsLast30Days"
+          :increase="state.newProductsAugmentation"
+        />
+        <MiniStatisticsCard
+          :icon="ShoppingBagIcon"
+          label="Nouvelles commandes"
+          :data="state.newOrdersLast30Days"
+          :increase="state.newOrdersAugmentation"
         />
       </dl>
     </div>
     <h4 class="mt-10 text-base font-medium leading-6 text-gray-900">Dernière année</h4>
     <div class="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2">
       <dl>
-        <div
-          class="relative overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:px-6 sm:pt-6"
-        >
+        <div class="relative overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:px-6 sm:pt-6">
           <Bar
             id="my-chart-id"
             :key="usersPerMonthKey"
@@ -126,12 +160,20 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
         </div>
       </dl>
       <dl>
-        <div
-          class="relative overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:px-6 sm:pt-6"
-        >
+        <div class="relative overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:px-6 sm:pt-6">
           <Bar
             id="my-chart-id"
-            :key="orderPerMonthKey"
+            :key="productsPerMonthKey"
+            :options="state.chartOptions"
+            :data="state.productChartData"
+          />
+        </div>
+      </dl>
+      <dl>
+        <div class="relative overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:px-6 sm:pt-6">
+          <Bar
+            id="my-chart-id"
+            :key="ordersPerMonthKey"
             :options="state.chartOptions"
             :data="state.orderChartData"
           />
