6e71361f1f7b8eafae897ff9d5b4fe538124ac91
riadh-fstck <r.benchouche1@gmail.com>
2023-10-29 11:46:24 +0100

    Add,edit a new category with sidebar, add a component table for categories and confirmation modal when deleting

--- dashboard/src/views/categories/CategoriesFormView.vue
+++ dashboard/src/views/categories/CategoriesFormView.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/layouts/AuthenticatedLayout.vue'
 import axiosInstance from '@/utils/axiosInstance';
-import axios from 'axios'
-import { reactive, ref } from 'vue'
+import {reactive, ref, watch} from 'vue'
+import ODrawer from "@/components/ODrawer.vue";
+
+const props = defineProps({
+  open: {
+    required: true,
+    type: Boolean,
+    default: false
+  },
+  id: {
+    required: false,
+    type: String,
+  },
+})
+
+const emit = defineEmits(['closeCreationDrawer', 'closeUpdatingDrawer'])
 
 
 const category = async () => {
@@ -23,45 +37,53 @@ const state = reactive({
 })
 
 const id = window.location.pathname.split('/').length > 3 ? window.location.pathname.split('/')[2] : null
-let submit = null;
-if(!id)
-{
-  submit = async () => {
-    try {
-      await axiosInstance.post('/categories', state.form)
-      window.location.href = '/categories';
-    } catch (e: any) {
-      state.errors = e.response.data.errors
-    }
-  }
-}else{
-  category();
-  submit = async () => {
-    try {
-      await axiosInstance.put(`/categories/${id}`, state.form)
-      window.location.href = '/categories';
-    } catch (e: any) {
-      state.errors = e.response.data.errors
-    }
+const submitCreation = async () => {
+  try {
+    await axiosInstance.post('/categories', state.form).then(
+        () => {
+          emit('closeCreationDrawer')
+        })
+  } catch (e: any) {
+    state.errors = e.response.data.errors
   }
 }
+const submitUpdating = async () => {
+  try {
+    await axiosInstance.put(`/categories/${props.id}`, state.form).then(
+        () => {
+          emit('closeUpdatingDrawer')
+        })
+  } catch (e: any) {
+    state.errors = e.response.data.errors
+  }
+}
+
+watch(() => props.id, async () => {
+  if (props.id) {
+    try {
+      const response = await axiosInstance.get(`/categories/${props.id}`)
+      state.form = response.data
+    } catch (e: any) {
+      throw e
+    }
+  }
+})
 
 </script>
 
 <template>
-  <AuthenticatedLayout>
-    <h3 v-if="!id">Create category</h3>
-    <h3 v-else>Edit Category {{ id }}</h3>
+  <ODrawer :open="props.open" @closeDrawer="!props.id ? emit('closeCreationDrawer') : emit('closeUpdatingDrawer')"
+           :title="!props.id ? 'Ajout d\'une nouvelle catégorie'  : `Modifier la catégorie ${ props.id }`">
     <div>
-      <form method="POST" class="space-y-6" @submit.prevent="submit">
+      <form method="POST" class="space-y-6" @submit.prevent="!props.id ? submitCreation() : submitUpdating()">
         <div>
           <FormKit
-            v-model="state.form.name"
-            type="text"
-            label="name"
-            validation="required"
-            placeholder="name"
-            :classes="{
+              v-model="state.form.name"
+              type="text"
+              label="name"
+              validation="required"
+              placeholder="name"
+              :classes="{
               label: 'block text-sm font-medium leading-6 text-gray-900',
               input:
                 'block w-full pl-2 rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
@@ -71,36 +93,27 @@ if(!id)
 
         <div>
           <FormKit
-            v-model="state.form.description"
-            type="text"
-            label="description"
-            validation="required|description"
-            placeholder="Write a description"
-            :classes="{
+              v-model="state.form.description"
+              type="text"
+              label="description"
+              validation="required|description"
+              placeholder="Write a description"
+              :classes="{
               label: 'block text-sm font-medium leading-6 text-gray-900',
               input:
                 'block w-full pl-2 rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
             }"
           />
         </div>
-
         <div>
           <button
-            type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            v-if="!id"
+              type="submit"
+              class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Create
-          </button>
-          <button
-            type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            v-else
-          >
-            Update
+            {{ !props.id ? 'Crée' : 'Modifier' }}
           </button>
         </div>
       </form>
     </div>
-  </AuthenticatedLayout>
+  </ODrawer>
 </template>
