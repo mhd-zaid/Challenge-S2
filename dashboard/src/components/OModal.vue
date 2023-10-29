24e4d2f614a2d4c57f1837499592ddb6935af414
riadh-fstck <r.benchouche1@gmail.com>
2023-10-29 11:47:02 +0100

    fix component modal and table

--- dashboard/src/components/OModal.vue
+++ dashboard/src/components/OModal.vue
@@ -62,7 +62,7 @@ const emit = defineEmits(['closeModal', 'confirm'])
                 </button>
                 <button type="button"
                         class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-                        @click="emit('closeModal')" ref="cancelButtonRef">Cancel
+                        @click="emit('closeModal')" ref="cancelButtonRef">Annuler
                 </button>
               </div>
             </DialogPanel>
--- dashboard/src/components/OTable.vue
+++ dashboard/src/components/OTable.vue
@@ -13,6 +13,8 @@ const props = defineProps({
     default: [] as unknown
   },
 })
+
+const emit = defineEmits(['showRow', 'updateRow', 'deleteRow'])
 </script>
 <template>
   <div class="mt-8 flow-root">
@@ -32,23 +34,26 @@ const props = defineProps({
             </tr>
             </thead>
             <tbody class="divide-y divide-gray-200 bg-white">
-            <tr v-for="(row, index ) in props.rows" :key="index">
+            <tr v-if="props.rows.length !== 0" v-for="(row, index ) in props.rows" :key="index">
               <td v-for="col in props.columns"
                   class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                 {{ row[col] }}
               </td>
               <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 space-x-4 items-baseline">
-                <button @click="$emit('showRow', row)">
+                <button @click="emit('showRow', row)">
                   <EyeIcon class="w-6 text-gray-900 hover:text-gray-950"/>
                 </button>
-                <button @click="$emit('updateRow', row)">
+                <button @click="emit('updateRow', row)">
                   <PencilSquareIcon class="w-6 text-orange-500 hover:text-orange-600"/>
                 </button>
-                <button @click="$emit('deleteRow', row)">
+                <button @click="emit('deleteRow', row)">
                   <TrashIcon class="w-6 text-red-600 hover:text-red-500"/>
                 </button>
               </td>
             </tr>
+            <div v-else>
+              <p class="text-center text-gray-500 text-xl font-semibold py-6">Aucune donnée disponible</p>
+            </div>
             </tbody>
           </table>
         </div>
