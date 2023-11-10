1c4c0fb5ef3da6992b8e5f72a8ccb93687572e25
riadh-fstck <r.benchouche1@gmail.com>
2023-11-10 16:52:27 +0100

    Fix layout

--- client/src/layout/FooterBar.vue
+++ client/src/layout/FooterBar.vue
@@ -1,35 +1,32 @@
 <script setup lang="ts">
 const footerNavigation = {
   shop: [
-    { name: 'Lifestyle', href: '#' },
-    { name: 'Football', href: '#' },
-    { name: 'Running', href: '#' },
-    { name: 'Chaussures de ville', href: '#' },
-    { name: 'Bottes', href: '#' }
+    { name: 'Lifestyle', href: '/products?category=lifestyle' },
+    { name: 'Football', href: '/products?category=football' },
+    { name: 'Running', href: '/products?category=running' },
+    { name: 'Chaussures de ville', href: '/products?category=ville' },
+    { name: 'Bottes', href: '/products?category=bottes' }
   ],
   company: [
-    { name: 'Qui nous sommes', href: '#' },
-    { name: 'Durabilité', href: '#' },
-    { name: 'Presse', href: '#' },
-    { name: 'Carrières', href: '#' },
-    { name: 'Conditions générales', href: '#' },
-    { name: 'Confidentialité', href: '#' }
+    { name: 'Qui nous sommes', href: '/about' },
+    { name: 'Conditions de ventes ', href: '/cgv' },
+    { name: `Conditions d'utilisation` , href: '/cgu' },
+    { name: 'FAQ', href: '/faq'}
   ],
   account: [
-    { name: 'Gérer le compte', href: '#' },
-    { name: 'Retours et échanges', href: '#' },
-    { name: 'Utiliser une carte-cadeau', href: '#' }
+    { name: 'Gérer le compte', href: '/profile' },
+    { name: 'Retours et échanges', href: '/exchange-policy' },
   ],
   connect: [
-    { name: 'Nous contacter', href: '#' },
-    { name: 'Twitter', href: '#' },
-    { name: 'Instagram', href: '#' },
-    { name: 'Pinterest', href: '#' }
+    { name: 'Nous contacter', href: '/contact' },
+    { name: 'Twitter', href: 'https://twitter.com/?lang=fr' },
+    { name: 'Instagram', href: 'https://www.instagram.com/' },
+    { name: 'Pinterest', href: 'https://www.pinterest.com/' }
   ]
 }
 </script>
 <template>
-  <footer aria-labelledby="footer-heading" class="bg-white">
+  <footer aria-labelledby="footer-heading" class="bg-white border-t border-gray-200">
     <h2 id="footer-heading" class="sr-only">Pied de page</h2>
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="py-20 xl:grid xl:grid-cols-3 xl:gap-8">
@@ -71,34 +68,12 @@ const footerNavigation = {
             </div>
           </div>
         </div>
-        <div class="mt-16 md:mt-16 xl:mt-0">
-          <h3 class="text-sm font-medium text-gray-900">Abonnez-vous à notre newsletter</h3>
-          <p class="mt-6 text-sm text-gray-500">
-            Les dernières offres et économies, envoyées dans votre boîte de réception chaque
-            semaine.
-          </p>
-          <form class="mt-2 flex sm:max-w-md">
-            <label for="adresse-email" class="sr-only">Adresse e-mail</label>
-            <input
-              id="adresse-email"
-              type="text"
-              autocomplete="email"
-              required
-              class="w-full min-w-0 appearance-none rounded-md border border-gray-300 bg-white px-4 py-2 text-base text-indigo-500 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
-            />
-            <div class="ml-4 flex-shrink-0">
-              <button
-                type="submit"
-                class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                S'inscrire
-              </button>
-            </div>
-          </form>
+        <div class="mt-16 xl:mt-0">
+          <img src="/images/sneakpeak_logo_black.png" alt="newsletter" class=" w-full"/>
         </div>
       </div>
 
-      <div class="border-t border-gray-200 py-10">
+      <div class="py-10">
         <p class="text-sm text-gray-500">Droits d'auteur &copy; 2023 Sneak Peak, Inc.</p>
       </div>
     </div>
