82e0e183b3502af086fa68476746dbc03b749337
Zaid MOUHAMAD <zaidmouhamad@gmail.com>
2023-12-01 23:57:26 +0100

    make sample pdf work

---
+++ dashboard/src/components/OrderPdf.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { defineProps} from 'vue'
+const props = defineProps(['data'])
+
+</script>
+<template>
+    <h1>Facture {{ props.data.id }}</h1>
+    <div>
+        Détails de l'utilisateur {{ props.data.userId }}
+        <div v-if="props.data.user">
+            <ul>
+                <li>
+                    Nom : {{ props.data.user.firstname }} - {{ props.data.user.lastname }}
+                </li>
+                <li>
+                    Adresse : {{ props.data.user.address }}
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div style="margin-bottom: 1in;">
+        Détails de la commande
+    </div>
+</template>
