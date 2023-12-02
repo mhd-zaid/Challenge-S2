7e3dab8a90e2eeec7daead7aacbd95f199a01ab7
mhd-zaid <zaidmouhamad@gmail.com>
2023-12-02 19:53:25 +0100

    add security for download invoice

--- dashboard/src/components/EntityDetailsTable.vue
+++ dashboard/src/components/EntityDetailsTable.vue
@@ -9,7 +9,7 @@ const props = defineProps(['data', 'entityType'])
 
 const downloadInvoice = async (orderId:string) => {
   const template = document.getElementById('invoice-template')
-  if(template){
+  if(template && (props.data.user.deletedAt === null) && (props.data.status !== 'payment pending') && (props.data.status !== 'payment failed')){
     template.style.display = 'block'
     await html2pdf(template, {
       margin: [1,1],
@@ -35,7 +35,7 @@ const downloadInvoice = async (orderId:string) => {
           </h1>
         </div>
         <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <button v-if="(props.entityType === 'order') && (props.data.status !== 'payment pending') && (props.data.status !== 'payment failed')"
+          <button v-if="(props.entityType === 'order') && (props.data.status !== 'payment pending') && (props.data.status !== 'payment failed') && (props.data.user.deletedAt === null)"
               v-on:click="downloadInvoice(props.data.id)"
               type="button"
               class="block rounded-md bg-red-700 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
